7b328c8a5cbc29ccfd17ec61d77a4f01bc4d1544
Shridhar Kumar <shriprem@gmail.com>
2020-09-13 11:16:55 -0400

    Moved UI element constant definitions into VisualizerUI.h

--- src/PluginDefinition.h
+++ src/PluginDefinition.h
@@ -26,6 +26,7 @@
 #include "NPP/PluginInterface.h"
 #include "ConfigIO.h"
 #include "Resources/resource.h"
+#include "Resources/VisualizerUI.h"
 #include "Resources/localization.h"
 
 const int nbFunc = 2;
--- src/Resources/resource.h
+++ src/Resources/resource.h
@@ -1486,17 +1486,6 @@
 #define LVS_NOSCROLL                    0x2000
 #define TVS_NOSCROLL                    0x2000
 #define TCS_OWNERDRAWFIXED              0x2000
-#define IDD_VISUALIZER_DOCKPANEL        9000
-#define IDC_VIZPANEL_FILETYPE_LABEL     9001
-#define IDC_VIZPANEL_FILETYPE_SELECT    9002
-#define IDC_VIZPANEL_CLEAR_BUTTON       9003
-#define IDC_VIZPANEL_FIELD_LABEL        9004
-#define IDC_VIZPANEL_FIELD_INFO         9005
-#define IDD_ABOUT_DIALOG                9200
-#define IDC_ABOUT_NAME                  9201
-#define IDC_ABOUT_VERSION               9202
-#define IDC_ABOUT_ATTRIBUTION           9203
-#define IDC_ABOUT_PROD_URL              9204
 #define CS_GLOBALCLASS                  0x4000
 #define TBSTYLE_REGISTERDROP            0x4000
 #define RBS_VERTICALGRIPPER             0x00004000
