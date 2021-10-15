48cf8738c69c675fe4f182c2c9eabd64c47077a2
shriprem <shriprem@gmail.com>
2021-10-14 20:48:10 -0400

    Unicode handling updates for Data Extraction

--- src/Dialogs/DataExtractDialog.cpp
+++ src/Dialogs/DataExtractDialog.cpp
@@ -624,6 +624,8 @@ void DataExtractDialog::extractData() {
    eolMarker = _configIO.getConfigStringA(fileType, L"RecordTerminator");
    eolMarkerLen = eolMarker.length();
 
+   bool byteCols{ _configIO.getConfigString(fileType, L"ByteColumns", L"Y") == L"Y" };
+
    const size_t endLine{ lineCount };
    for (size_t currentLine{}; currentLine < endLine; currentLine++) {
       if (sciFunc(sciPtr, SCI_LINELENGTH, currentLine, NULL) > FW_LINE_MAX_LENGTH) {
@@ -679,8 +681,17 @@ void DataExtractDialog::extractData() {
          LineItemInfo& LI = validLIs[j];
          if (static_cast<int>(regexIndex) != LI.recType) continue;
 
-         sciTR.chrg.cpMin = static_cast<long>(recStartPos + recInfoList[LI.recType].fieldStarts[LI.fieldType]);
-         sciTR.chrg.cpMax = sciTR.chrg.cpMin + recInfoList[LI.recType].fieldWidths[LI.fieldType];
+         if (byteCols) {
+            sciTR.chrg.cpMin = static_cast<long>(recStartPos + recInfoList[LI.recType].fieldStarts[LI.fieldType]);
+            sciTR.chrg.cpMax = sciTR.chrg.cpMin + recInfoList[LI.recType].fieldWidths[LI.fieldType];
+         }
+         else {
+            sciTR.chrg.cpMin = static_cast<long>(sciFunc(sciPtr, SCI_POSITIONRELATIVE,
+               (WPARAM)recStartPos, (LPARAM)recInfoList[LI.recType].fieldStarts[LI.fieldType]));
+            sciTR.chrg.cpMax = static_cast<long>(sciFunc(sciPtr, SCI_POSITIONRELATIVE,
+               (WPARAM)sciTR.chrg.cpMin, (LPARAM)recInfoList[LI.recType].fieldWidths[LI.fieldType]));
+         }
+
          sciFunc(sciPtr, SCI_GETTEXTRANGE, NULL, (LPARAM)&sciTR);
 
          extract += validLIs[j].prefix + fieldText + validLIs[j].suffix;
